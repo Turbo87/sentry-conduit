e0075069e629348cae26a8b312fe19a3d1605feb
Tobias Bieniek <tobias@bieniek.cloud>
2021-10-07 11:23:58 +0200

    Include URL and HTTP method in events

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,7 @@
-use conduit::{Handler, RequestExt};
+use conduit::{Handler, Host, RequestExt, Scheme};
 use conduit_middleware::{AfterResult, AroundMiddleware};
+use sentry_core::protocol::{Event, Request};
+use std::borrow::Cow;
 
 pub struct SentryMiddleware {
     handler: Option<Box<dyn Handler>>,
@@ -22,6 +24,13 @@ impl Handler for SentryMiddleware {
         sentry_core::with_scope(
             |_scope| {},
             || {
+                let sentry_req = sentry_request_from_http(req);
+                sentry_core::configure_scope(|scope| {
+                    scope.add_event_processor(Box::new(move |event| {
+                        Some(process_event(event, &sentry_req))
+                    }))
+                });
+
                 let result = self.handler.as_ref().unwrap().call(req);
                 if let Err(error) = &result {
                     sentry_core::capture_error(error.as_ref());
@@ -30,4 +39,42 @@ impl Handler for SentryMiddleware {
             },
         )
     }
+}
+
+/// Build a Sentry request struct from the HTTP request
+fn sentry_request_from_http(request: &dyn RequestExt) -> Request {
+    let method = Some(request.method().to_string());
+
+    let scheme = match request.scheme() {
+        Scheme::Http => "http",
+        Scheme::Https => "https",
+    };
+
+    let host = match request.host() {
+        Host::Name(name) => Cow::from(name),
+        Host::Socket(addr) => Cow::from(addr.to_string()),
+    };
+
+    let path = request.path();
+
+    let mut url = format!("{}://{}{}", scheme, host, path);
+
+    if let Some(query_string) = request.query_string() {
+        url += "?";
+        url += query_string;
+    }
+
+    Request {
+        url: url.parse().ok(),
+        method,
+        ..Default::default()
+    }
+}
+
+/// Add request data to a Sentry event
+fn process_event(mut event: Event<'static>, request: &Request) -> Event<'static> {
+    if event.request.is_none() {
+        event.request = Some(request.clone());
+    }
+    event
 }
